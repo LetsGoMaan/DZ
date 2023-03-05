a391822658bc9905309c4b7d1ef4300df4cd87f2
LetsGoMan <ilikeseik@gmail.com>
2023-03-05 07:45:12 +0300

    finish seven and eight dz

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import s2 from '../../s1-main/App.module.css'
-import Stand from './Stand'
+import React from "react"
+import s2 from "../../s1-main/App.module.css"
+import Stand from "./Stand"
 
 /*
 * 1 - понять (и простить) SuperInputText
@@ -11,13 +11,14 @@ import Stand from './Stand'
 
 const HW4 = () => {
     return (
-        <div id={'hw4'}>
+        <div id={"hw4"}>
             <div className={s2.hwTitle}>Homework #4</div>
             <div className={s2.hwLine5}></div>
             {/*демонстрация возможностей компонент:*/}
             <div className={s2.hw}>
                 <Stand/>
             </div>
+            <div className={s2.hwLine5}></div>
         </div>
     )
 }
--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import SuperSelect from './common/c5-SuperSelect/SuperSelect'
-import SuperRadio from './common/c6-SuperRadio/SuperRadio'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW7.module.css'
+import React, {useState} from "react"
+import SuperSelect from "./common/c5-SuperSelect/SuperSelect"
+import SuperRadio from "./common/c6-SuperRadio/SuperRadio"
+import s2 from "../../s1-main/App.module.css"
+import s from "./HW7.module.css"
 
 /*
 * 1 - в файле SuperSelect.tsx дописать логику функции onChangeCallback
@@ -12,16 +12,16 @@ import s from './HW7.module.css'
 * */
 
 const arr = [
-    { id: 1, value: 'x' },
-    { id: 2, value: 'y' },
-    { id: 3, value: 'z' },
+    {id: 1, value: "Pre-junior"},
+    {id: 2, value: "Junior"},
+    {id: 3, value: "Junior +"},
 ] // value может быть изменено
 
 const HW7 = () => {
     const [value, onChangeOption] = useState(1) // селект и радио должны работать синхронно
 
     return (
-        <div id={'hw7'}>
+        <div id={"hw7"}>
             <div className={s2.hwTitle}>Homework #7</div>
 
             {/*демонстрация возможностей компонент:*/}
@@ -29,16 +29,16 @@ const HW7 = () => {
                 <div className={s.container}>
                     <div>
                         <SuperSelect
-                            id={'hw7-super-select'}
+                            id={"hw7-super-select"}
                             options={arr}
-                            value={value}
+                            value={value} /// Choice
                             onChangeOption={onChangeOption}
                         />
                     </div>
                     <div>
                         <SuperRadio
-                            id={'hw7-super-radio'}
-                            name={'hw7-radio'}
+                            id={"hw7-super-radio"}
+                            name={"hw7-radio"}
                             options={arr}
                             value={value}
                             onChangeOption={onChangeOption}
